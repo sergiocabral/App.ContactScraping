c1e65085605cab6e7f3566a2e1252f27faf47c0d
Sergio Cabral <git@sergiocabral.com>
2021-09-14 13:24:07 -0300

    code: FactoryUrl::googleSearch funcionando

--- index.php
+++ index.php
@@ -1,8 +1,35 @@
 <?php
 
+/**
+ * Geral url de serviços de internet.
+ */
+class FactoryUrl {
+    /**
+     * Url para pesquisa no Google.
+     * @param string $term Termo de pesquisa.
+     * @return string Url montada.
+     */
+    public static function googleSearch(string $term): string
+    {
+        $termEncoded = urlencode($term);
+        return "https://www.google.com/search" .
+        "?q=$termEncoded" .
+        "&rlz=1C1GCEU_pt-BRBR888BR888" .
+        "&sxsrf=AOaemvIElWeAOvOD_ol-57QS-VqMOcUvDA%3A1631635328965" .
+        "&ei=gMdAYfyuOvDF5OUPpY-fqAk" .
+        "&oq=$termEncoded" .
+        "&gs_lcp=Cgdnd3Mtd2l6EAMyBAgjECcyBAgjECcyCAguEIAEELEDMgQILhBDMgsILhCABBDHARDRAzIFCAAQgAQyBQgAEIAEMggIABCABBCxAzIFCAAQgAQyBQgAEIAEOgcIABBHELADOhQILhCABBCxAxCDARDHARDRAxCTAjoLCAAQgAQQsQMQgwE6DgguEIAEELEDEMcBEKMCOgsILhCABBCxAxCDAToICAAQsQMQgwE6CAguELEDEIMBOgQIABBDOgoILhDHARDRAxBDOg4ILhCABBCxAxDHARDRAzoKCC4QsQMQgwEQQzoHCC4QsQMQQ0oECEEYAFD81DRY8to0YJTdNGgDcAJ4AIABiQGIAYgFkgEDMC41mAEAoAEByAEIuAECwAEB" .
+        "&sclient=gws-wiz" .
+        "&ved=0ahUKEwj87r2h6_7yAhXwIrkGHaXHB5UQ4dUDCA4" .
+        "&uact=5";
+    }
+}
+
 class Main {
+
     public function run() {
-        echo "Hello World";
+        $url = FactoryUrl::googleSearch('pet shop');
+        echo $url;
     }
 }
 
